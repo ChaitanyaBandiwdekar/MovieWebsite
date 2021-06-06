0a600d5a398f0d7b9db80dc4611cdd3128917d78
“ChaitanyaBandiwdekar” <c.bandiwdekar@somaiya.edu>
2021-06-06 20:43:30 +0530

    db connection added

--- moviedb/models.py
+++ moviedb/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Movie:
-    img: str
-    name: str
-    release_date = int
+class Movie(models.Model):
+    img = models.ImageField(upload_to='pics')
+    name = models.CharField(max_length=100)
+    release_date = models.IntegerField()
 
-    def __init__(self, img, name, release_date) -> None:
-        self.img = img
-        self.name = name
-        self.release_date = release_date
+    # def __init__(self, img, name, release_date) -> None:
+    #     self.img = img
+    #     self.name = name
+    #     self.release_date = release_date
--- moviedb/views.py
+++ moviedb/views.py
@@ -4,15 +4,6 @@ from .models import Movie
 # Create your views here.
 def home(request):
 
-    movies = []
-
-    movie1 = Movie('img1.jpeg', 'Black Panther', 2018)
-    movie2 = Movie('img2.jpeg', 'After', 2017)
-    movie3 = Movie('img3.jpeg', 'Joker', 2019)
-    movie4 = Movie('img4.jpeg', 'Spiderman', 2017)
-    movie5 = Movie('img5.jpeg', 'Assassin', 2014)
-    movie6 = Movie('img6.jpeg', 'Captain America', 2015)
-
-    movies = [movie1, movie2, movie3, movie4, movie5, movie6]
+    movies = Movie.objects.all()
 
     return render(request, 'cap_homepage.html', {'movies': movies})
